2d39163d71778eef4f49d6ecdb38565782f6365d
jonas <jonasbhjulstad@gmail.com>
2024-05-29 16:58:45 +0200

    Implementation of Player Controller + partial implementation of following camera

---
+++ camera_follow.gd
@@ -0,0 +1,24 @@
+
+
+extends Camera3D
+
+@export var distance = 4.0
+@export var height = 2.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_physics_process(true)
+	
+	set_as_top_level(true)
+
+func _physics_process(delta):
+	var target = get_parent().get_global_transform().origin
+	var pos = get_global_transform().origin
+	var up = Vector3(0,1,0)
+
+	var offset = pos - target
+	
+	offset = offset.normalized()*distance
+	offset.y = height
+	
+	pos = target + offset
+	look_at_from_position(pos, target, up)
