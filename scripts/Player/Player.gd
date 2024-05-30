776f402475290df9c616483fa98e9d28311db504
jonas <jonasbhjulstad@gmail.com>
2024-05-30 18:02:58 +0200

    Camera following Character transform.origin

--- scripts/Player/Player.gd
+++ scripts/Player/Player.gd
@@ -6,7 +6,12 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+var anim_player
+var character
+var camera
+func _ready():
+	#anim_player = get_node("AnimationPlayer")
+	character = get_node(".")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,7 +24,9 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# var input_dir = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+	# get a, d, s, w
+	var input_dir = Input.get_vector("move_right", "move_left", "move_backward", "move_forward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -27,5 +34,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		
+	#update global transform
+	character.global_transform.origin += velocity * delta
 	move_and_slide()
--- scripts/camera.gd
+++ scripts/camera.gd
@@ -11,24 +11,72 @@ const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
 var _mouse_position = Vector2(0.0, 0.0)
 var _total_pitch = 0.0
 
-# Movement state
-var _direction = Vector3(0.0, 0.0, 0.0)
-var _velocity = Vector3(0.0, 0.0, 0.0)
+var distance_to_character = 5.0
+
 var _acceleration = 30
 var _deceleration = -10
 var _vel_multiplier = 4
+var _lookaround = false
 
-# Keyboard state
-var _w = false
-var _s = false
-var _a = false
-var _d = false
-var _q = false
-var _e = false
-var _shift = false
-var _alt = false
+var _reference_pos = Vector3()
+var _reference_angle = Vector3(30, 180, 0)
+
+var _velocity = Vector3()
+var _omega = Vector3()
+var _pos_epsilon = Vector3()
+var _angle_epsilon = Vector3()
+
+
+func get_player_cam_reference_offset():
+	var z_offset = cos(deg_to_rad(_reference_angle[0])) * distance_to_character
+	var y_offset = sin(deg_to_rad(_reference_angle[0])) * distance_to_character
+	return Vector3(0, y_offset, -z_offset)
+
+func position_pid(target, current, delta):
+	var kp = 0.1
+	var ki = 0.1
+	var kd = 0.1
+	var error = target - current
+	var integral = error * delta
+	var derivative = (error - _pos_epsilon) / delta
+	_pos_epsilon = error
+	return kp * error + ki * integral + kd * derivative
+
+func angle_pid(target, current, delta):
+	var kp = 0.1
+	var ki = 0.1
+	var kd = 0.1
+	var error = target - current
+	var integral = error * delta
+	var derivative = (error - _angle_epsilon) / delta
+	_angle_epsilon = error
+	return kp * error + ki * integral + kd * derivative
+
+func get_looking_at_angle():
+	var looking_at = get_parent().global_transform.origin
+	var direction = looking_at - global_transform.origin
+	var yaw = atan2(direction.x, direction.z)
+	var pitch = atan2(direction.y, sqrt(direction.x * direction.x + direction.z * direction.z))
+	return Vector3(rad_to_deg(pitch), rad_to_deg(yaw), 0)
+
+func _ready():
+	# Set the mouse mode to captured
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	var p_origin = get_parent().global_transform.origin
+
+	global_transform.origin = p_origin + get_player_cam_reference_offset()
+	Basis().looking_at(p_origin, Vector3(0, 1, 0))
+	_reference_angle = Basis().get_euler()
+
 
 func _input(event):
+
+	#get position of parent node
+
+	#check scroll wheel
+	_lookaround = Input.is_action_pressed("lookaround")
+	
 	# Receives mouse motion
 	if event is InputEventMouseMotion:
 		_mouse_position = event.relative
@@ -43,74 +91,29 @@ func _input(event):
 			MOUSE_BUTTON_WHEEL_DOWN: # Decereases max velocity
 				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 20)
 
-	# Receives key input
-	if event is InputEventKey:
-		match event.keycode:
-			KEY_W:
-				_w = event.pressed
-			KEY_S:
-				_s = event.pressed
-			KEY_A:
-				_a = event.pressed
-			KEY_D:
-				_d = event.pressed
-			KEY_Q:
-				_q = event.pressed
-			KEY_E:
-				_e = event.pressed
-			KEY_SHIFT:
-				_shift = event.pressed
-			KEY_ALT:
-				_alt = event.pressed
-
 # Updates mouselook and movement every frame
 func _process(delta):
-	_update_mouselook()
-	_update_movement(delta)
 
-# Updates camera movement
-func _update_movement(delta):
-	# Computes desired direction from key states
-	_direction = Vector3(
-		(_d as float) - (_a as float), 
-		(_e as float) - (_q as float),
-		(_s as float) - (_w as float)
-	)
-	
-	# Computes the change in velocity due to desired direction and "drag"
-	# The "drag" is a constant acceleration on the camera to bring it's velocity to 0
-	var offset = _direction.normalized() * _acceleration * _vel_multiplier * delta \
-		+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
-	
-	# Compute modifiers' speed multiplier
-	var speed_multi = 1
-	if _shift: speed_multi *= SHIFT_MULTIPLIER
-	if _alt: speed_multi *= ALT_MULTIPLIER
-	
-	# Checks if we should bother translating the camera
-	if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
-		# Sets the velocity to 0 to prevent jittering due to imperfect deceleration
-		_velocity = Vector3.ZERO
-	else:
-		# Clamps speed to stay within maximum value (_vel_multiplier)
-		_velocity.x = clamp(_velocity.x + offset.x, -_vel_multiplier, _vel_multiplier)
-		_velocity.y = clamp(_velocity.y + offset.y, -_vel_multiplier, _vel_multiplier)
-		_velocity.z = clamp(_velocity.z + offset.z, -_vel_multiplier, _vel_multiplier)
-	
-		translate(_velocity * delta * speed_multi)
+
+	_reference_pos = get_parent().global_transform.origin + get_player_cam_reference_offset()
+	_reference_angle = get_looking_at_angle() + get_lookaround_angle_offset()
+
+	global_transform.origin = position_pid(_reference_pos, global_transform.origin, delta)
+
 
 # Updates mouse look 
-func _update_mouselook():
+func get_lookaround_angle_offset():
 	# Only rotates mouse if the mouse is captured
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+	if _lookaround:
 		_mouse_position *= sensitivity
 		var yaw = _mouse_position.x
 		var pitch = _mouse_position.y
-		_mouse_position = Vector2(0, 0)
-		
-		# Prevents looking up/down too far
-		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
-		_total_pitch += pitch
-	
-		rotate_y(deg_to_rad(-yaw))
-		rotate_object_local(Vector3(1,0,0), deg_to_rad(-pitch))
+		pitch = clamp(pitch, -89, 89)
+		yaw = fmod(yaw, 360)
+		#print pitch yaw
+		# print(Vector3(pitch, yaw, 0))
+
+
+		return Vector3(pitch, yaw, 0)
+	else:
+		return Vector3(0, 0, 0)
